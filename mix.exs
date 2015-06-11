de617109235153dd95fe4d58ec69edd45eca1c7d
Ross Jones <ross@servercode.co.uk>
2015-06-11 18:22:10 +0100

    Useful config just in case I end up publishing this on hex

--- mix.exs
+++ mix.exs
@@ -4,28 +4,38 @@ defmodule Nifty.Mixfile do
   def project do
     [app: :nifty,
      version: "0.0.1",
+     description: description,
      elixir: "~> 1.0",
+     name: "Nifty",
+     source_url: "https://github.com/rossjones/nifty",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
+  defp package do
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     contributors: ["Ross Jones"],
+     licenses: ["Apache 2.0"],
+     links: %{
+       github: "https://github.com/rossjones/nifty",
+     }]
+  end
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  def description do
+    """
+    A semi-useful tool to generate boilerplate when you want to
+    use a NIF in your project. You don't really want to use a NIF
+    in your project, until you do. Then this might save you writing
+    all the boilerplate. Perhaps.
+    """
+  end
+
   defp deps do
     []
   end
